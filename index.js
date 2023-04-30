67b5382a0972aacf8ea2587ef1094326962bfab7
iryna-zhyhala <iryna.zhyhala@gmail.com>
2023-05-01 00:54:01 +0300

    feat: add texterea

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-let keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, 9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 59, 93, 92, 46, 20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 59, 39, 13, 16, 90, 88, 67, 86, 66, 78, 77, 44, 46, 47, 38, 16, 17, 91, 18, 32, 18, 37, 40, 39, 17];
+let body = document.getElementsByTagName('body')
+let title = document.createElement('p');
+title.className = "titles";
+title.innerHTML = "RSS Virtual Keyboard";
+document.body.prepend(title);
+
+let keyboard = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, 9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 59, 93, 92, 46, 20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 59, 39, 13, 16, 90, 88, 67, 86, 66, 78, 77, 44, 46, 47, 38, 16, 17, 91, 18, 32, 18, 37, 40, 39, 17];
 
 //document.onkeydown = function(event) {
 //  console.log(event);
@@ -16,7 +22,6 @@ function init() {
     }
     document.querySelector('#keyboard').innerHTML = out;
 }
-
 init();
 
 document.onkeydown = function(event) {
